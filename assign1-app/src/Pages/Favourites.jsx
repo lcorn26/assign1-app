8f30602547869c3f186ba4acab62332fee88711d
lcorn26 <lcorn268@mtroyal.ca>
2021-11-08 16:36:03 -0700

    added favourites page

---
+++ assign1-app/src/Pages/Favourites.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+
+export function Favourites() {
+    return (
+        <div >
+            <h1>Favourites</h1>
+        </div>
+    )
+}
