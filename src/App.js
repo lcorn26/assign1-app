fee1b2529e3211b04217acee1f4d680ea3035271
lcorn26 <lcorn268@mtroyal.ca>
2021-11-08 23:58:21 -0700

    Move files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Routings } from './Routes/Routes';
+import Modal from 'react-modal';
+
+function App() {
+  return (
+    <div className="App">
+     <Routings/>
+    </div>
+  );
+}
+
+export default App;
+
