71c9c73737c25b023fb0fee1e4835e1336f4ac14
lcorn26 <lcorn268@mtroyal.ca>
2021-11-08 22:50:50 -0700

    credit

--- assign1-app/src/Pages/Home.jsx
+++ assign1-app/src/Pages/Home.jsx
@@ -58,7 +58,9 @@ export const Home = () => {
                                     </Button></Link>
                                 </Col>
                             </Row>
+                            
                         </Form.Item>
+                        <div id="credit">Image retrieved from NIKON CORPORATION, NIKON D750 Published on December 26, 2017</div>
                     </Form>
                 </Col>
             </Row>
